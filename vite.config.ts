6c7022e9f4943ed6872a7605a80a21ed96488325
Jashoor Kingsly <jk2529@srmist.edu.in>
2025-05-08 03:47:14 +0530

    Jk

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Jashoor/AccAI-FSD.git',  // Replace with your actual GitHub repository name
+  base: '/AccAI-FSD/',  // Replace with your actual GitHub repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
